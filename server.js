8b393d77129771bc7e343cca7888c71e16bc90f0
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2021-09-21 20:43:33 -0400

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/k9cs'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname + '/dist/k9cs/src/index.html'));});
+app.listen(process.env.PORT || 8080);
